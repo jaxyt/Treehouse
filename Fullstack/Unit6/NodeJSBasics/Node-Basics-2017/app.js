ba7536333d8313707649958388fd77bd1c1e9a95
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-06-25 21:00:27 -0400

    first node program prep

--- Fullstack/Unit6/NodeJSBasics/Node-Basics-2017/app.js
+++ Fullstack/Unit6/NodeJSBasics/Node-Basics-2017/app.js
@@ -1,3 +1,12 @@
-console.log("hello world");
-console.error('this is for logging error messages');
-console.dir({name: "Jax", age: "23"});
+// console.log("hello world");
+// console.error('this is for logging error messages');
+// console.dir({name: "Jax", age: "23"});
+
+//Problem: we need a simple way to look at a users badge count and javascript points
+//Solution: use node.js to connect to treehouses api to get profile information to print out
+
+
+//connect to the api url (https://teamtreehouse.com/jaxtaylor.json)
+//Read the data
+//Parse the data
+//print the data
