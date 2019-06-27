670f683e94fac880a34b6023ae9eb541a9a5f651
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-06-27 18:39:56 -0400

    did a lot of stuff with npm

--- Fullstack/Unit6/NPMBasics/BcryptTutorial/app.js
+++ Fullstack/Unit6/NPMBasics/BcryptTutorial/app.js
@@ -1,10 +1,12 @@
 const bcrypt = require('bcrypt');
+const colors = require('colors');
 const saltRounds = 10;
 var myPlaintextPassword = "password";
 var hashedPassword;
 var someOtherPlaintextPassword = 'wrongpass'
 
 bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
+    console.log(hash.green)
     bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
         console.log(res);
     });
