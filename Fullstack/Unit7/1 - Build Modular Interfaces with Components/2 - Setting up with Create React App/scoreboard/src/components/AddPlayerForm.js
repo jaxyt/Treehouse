85abb71b6ff2ee3fbc77b5ce4a6769b77cc82d7d
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-07-08 15:33:40 -0400

    react components

--- Fullstack/Unit7/1 - Build Modular Interfaces with Components/2 - Setting up with Create React App/scoreboard/src/components/AddPlayerForm.js
+++ Fullstack/Unit7/1 - Build Modular Interfaces with Components/2 - Setting up with Create React App/scoreboard/src/components/AddPlayerForm.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddPlayerForm extends Component {
     
-    state = {
-        value: ''
-    }
+    
+    playerInput = React.createRef();
 
-    handleValueChange = (e) => {
-        this.setState({ value: e.target.value });
-    }
+
     
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.addPlayer(this.state.value);
-        this.setState({value: ''});
+        this.props.addPlayer(this.playerInput.current.value);
+        e.currentTarget.reset();
     }
 
     render() {
+        const {handleSubmit} = this;
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
-                    value={this.state.value}
-                    onChange={this.handleValueChange}
+                    ref={this.playerInput}
                     placeholder="Enter a player's name"
                 />
 
@@ -35,5 +33,9 @@ class AddPlayerForm extends Component {
     };
 };
 
+
+AddPlayerForm.propTypes = {
+    addPlayer: PropTypes.func
+}
 
 export default AddPlayerForm;
